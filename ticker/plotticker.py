4b4ec7de9bbe1177c792b9d3445e3c83599aa258
yatin-kapur <yatin.aol@gmail.com>
2017-07-09 16:02:59 -0400

    matplotlib fix

--- ticker/plotticker.py
+++ ticker/plotticker.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-matplotlib.use('Agg')
 import matplotlib.dates as mdates
+matplotlib.use('Agg')
 import urllib
 import datetime
 
